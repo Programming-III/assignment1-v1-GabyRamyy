bb1968cb89cbacff332c92a12744c6b965152389
GabyRamyy <gaby.ramy2006@gmail.com>
2025-11-12 15:23:59 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -1,13 +1,23 @@
 #ifndef MAMMAL_H
 #define MAMMAL_H
-
+#include <string>
 #include "Animal.h"
+using namespace std;
 
-//define mammal class here
+class Mammal: public Animal{
+
+    private:
+        string furColor;
+    public:
+        Mammal();
+        Mammal(string name, int age, bool isHungry,string furC);
+        ~Mammal();
+        void display() override;
+        void setFurColor(string fur);
+        string getFurColor();
+        
 
 
-
-
-
+};
 
 #endif
