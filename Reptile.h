b1e879cef1588ae447f44351c08c5ebd72d5bbd9
GabyRamyy <gaby.ramy2006@gmail.com>
2025-11-12 15:21:33 +0200

    Refactor Reptile.h header guard and include directives

--- Reptile.h
+++ Reptile.h
@@ -1,16 +1,22 @@
-#ifndef REPTILE_H
-#define REPTILE_H
-
+#ifndef  REPTILE_H
+#define  REPTILE_H
+#include <string>
 #include "Animal.h"
+using namespace std;
 
-//define reptile class here
+class Reptile: public Animal{
+
+    private:
+        bool isVenomous;
+    public:
+        Reptile();
+        Reptile(string name, int age, bool isHungry,bool isVe);
+        ~Reptile();
+        void setIsVenomous(bool isV);
+        string getIsVenomous();
+        void display() override;
 
 
-
-
-
-
-
-
+};
 
 #endif
